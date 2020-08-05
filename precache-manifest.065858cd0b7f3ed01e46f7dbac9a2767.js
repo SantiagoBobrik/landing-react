eae65c299ccc3fcfb91a7a9b580efe2c76b0a1de
SantiagoBobrik <santiago.bobrik@gmail.com>
2020-08-05 19:53:45 -0300

    Updates

---
+++ precache-manifest.065858cd0b7f3ed01e46f7dbac9a2767.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2ffc1ccd2f33f79d1c5734437107572f",
+    "url": "/landing-react/index.html"
+  },
+  {
+    "revision": "35a19c6d3bffbca4524d",
+    "url": "/landing-react/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "d59b4f85e75bd3e54432",
+    "url": "/landing-react/static/css/main.9e239999.chunk.css"
+  },
+  {
+    "revision": "35a19c6d3bffbca4524d",
+    "url": "/landing-react/static/js/2.b27b4ce2.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/landing-react/static/js/2.b27b4ce2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d59b4f85e75bd3e54432",
+    "url": "/landing-react/static/js/main.cc326fa7.chunk.js"
+  },
+  {
+    "revision": "fdca0907b9b2b88e9dac",
+    "url": "/landing-react/static/js/runtime-main.f9ded1c0.js"
+  },
+  {
+    "revision": "3696807757a5f7ace69e2dcd2101df55",
+    "url": "/landing-react/static/media/bomb.36968077.gif"
+  },
+  {
+    "revision": "0acbe3edfd4bee7c1b802aad0cbdc873",
+    "url": "/landing-react/static/media/coronavirus.0acbe3ed.svg"
+  },
+  {
+    "revision": "0fef6a452f49b03139d93fb5f7714d26",
+    "url": "/landing-react/static/media/dinero.0fef6a45.svg"
+  },
+  {
+    "revision": "c63fd3f8a97735b78feb7d49fcd3e776",
+    "url": "/landing-react/static/media/logo.c63fd3f8.png"
+  }
+]);
